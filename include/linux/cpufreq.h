ddb6e0128b5b4a7acf14a6eff0adcca4c8a4b1f3
Ezekeel <notezekeel@googlemail.com>
2012-01-12 18:44:05 +0100

    Added CPUfreq governor 'lazy'.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -364,6 +364,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
+extern struct cpufreq_governor cpufreq_gov_lazy;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lazy)
 #endif
 
 
