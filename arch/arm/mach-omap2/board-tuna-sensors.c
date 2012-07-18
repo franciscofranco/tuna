b4b6a2b7084cb2da12d49d6ceb14c577423f6a6b
franciscofranco <franciscofranco.1990@gmail.com>
2012-07-18 05:28:17 +0100

    board-tuna-sensors.c: merge Kalim fix to toroplus compass bug.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-omap2/board-tuna-sensors.c
+++ arch/arm/mach-omap2/board-tuna-sensors.c
@@ -170,7 +170,11 @@ static void omap4_tuna_fixup_orientations_maguro(int revision)
 
 static void omap4_tuna_fixup_orientations_toro(int revision)
 {
-	if (revision >= 2) {
+	if (revision >= 14) {
+		rotcpy(mpu_data.orientation, orientation_back_left_90);
+		rotcpy(mpu_data.accel.orientation, orientation_back);
+		rotcpy(mpu_data.compass.orientation, orientation_back);
+	} else if (revision >= 2) {
 		rotcpy(mpu_data.orientation, orientation_back_left_90);
 		rotcpy(mpu_data.accel.orientation, orientation_back);
 		rotcpy(mpu_data.compass.orientation, orientation_back_180);
