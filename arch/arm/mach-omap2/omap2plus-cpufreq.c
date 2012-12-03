dd5d997878aacea799cd8f691027f89dbc1daafc
franciscofranco <franciscofranco.1990@gmail.com>
2012-12-02 20:46:43 -0800

    Decrease 192MHz voltage, increase voltage on > 1,4GHz freqs. Also fix a typo, we want to boot with 1228800 not 1228000.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -344,7 +344,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
 	policy->min = 384000;
-	policy->max = 1228000;
+	policy->max = 1228800;
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
