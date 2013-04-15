67925d5a6f9e3a60fd09988528f0d8a35bb9a6d6
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-15 16:10:14 -0700

    block: row: hardcode values research by the community, you guys know who you are.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -61,17 +61,17 @@ static const bool queue_idling_enabled[] = {
 /* Default values for row queues quantums in each dispatch cycle */
 static const int queue_quantum[] = {
 	100,	/* ROWQ_PRIO_HIGH_READ */
-	100,	/* ROWQ_PRIO_REG_READ */
-	2,	/* ROWQ_PRIO_HIGH_SWRITE */
-	1,	/* ROWQ_PRIO_REG_SWRITE */
-	1,	/* ROWQ_PRIO_REG_WRITE */
-	1,	/* ROWQ_PRIO_LOW_READ */
-	1	/* ROWQ_PRIO_LOW_SWRITE */
+	75,	/* ROWQ_PRIO_REG_READ */
+	5,	/* ROWQ_PRIO_HIGH_SWRITE */
+	4,	/* ROWQ_PRIO_REG_SWRITE */
+	4,	/* ROWQ_PRIO_REG_WRITE */
+	3,	/* ROWQ_PRIO_LOW_READ */
+	12	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues */
-#define ROW_IDLE_TIME_MSEC 5	/* msec */
-#define ROW_READ_FREQ_MSEC 20	/* msec */
+#define ROW_IDLE_TIME_MSEC 10	/* msec */
+#define ROW_READ_FREQ_MSEC 25	/* msec */
 
 /**
  * struct rowq_idling_data -  parameters for idling on the queue
