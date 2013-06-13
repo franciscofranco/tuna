370939a682b8631479d69ed5fd67395307c0a99e
Boy Petersen <boypetersen@gmail.com>
2013-06-13 16:08:47 +0200

    block/bfq-iosched.c: BFQ scheduler tweaks (thanks pkgnex from Franco's Dev Team)
    Also fixed slice_idle limitation (credits to me)
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -74,10 +74,10 @@ static const int bfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
 static const int bfq_back_max = 16 * 1024;
 
 /* Penalty of a backwards seek, in number of sectors. */
-static const int bfq_back_penalty = 2;
+static const int bfq_back_penalty = 1;
 
 /* Idling period duration, in jiffies. */
-static int bfq_slice_idle = HZ / 125;
+static int bfq_slice_idle = 0;
 
 /* Default maximum budget values, in sectors and number of requests. */
 static const int bfq_default_max_budget = 16 * 1024;
@@ -3228,7 +3228,7 @@ static int __init bfq_init(void)
 	/*
 	 * Can be 0 on HZ < 1000 setups.
 	 */
-	if (bfq_slice_idle == 0)
+	if (bfq_slice_idle == 0 && HZ >= 1000)
 		bfq_slice_idle = 1;
 
 	if (bfq_timeout_async == 0)
