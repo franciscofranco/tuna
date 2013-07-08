f1406d1447a6d73c7770521f241498b373546e4d
Francisco Franco <franciscofranco.1990@gmail.com>
2013-07-08 02:45:07 +0100

    block: deadline: change the winning tunables again.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,10 +17,10 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = HZ / 2;  /* max time before a read is submitted. */
-static const int write_expire = 5 * HZ; /* ditto for writes, these limits are SOFT! */
-static const int writes_starved = 4;    /* max times reads can starve a write */
-static const int fifo_batch = 1;       /* # of sequential requests treated as one
+static const int read_expire = 32;  /* max time before a read is submitted. */
+static const int write_expire = 320; /* ditto for writes, these limits are SOFT! */
+static const int writes_starved = 1;    /* max times reads can starve a write */
+static const int fifo_batch = 8;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
 struct deadline_data {
@@ -354,7 +354,7 @@ static void *deadline_init_queue(struct request_queue *q)
 	dd->fifo_expire[READ] = read_expire;
 	dd->fifo_expire[WRITE] = write_expire;
 	dd->writes_starved = writes_starved;
-	dd->front_merges = 0;
+	dd->front_merges = 1;
 	dd->fifo_batch = fifo_batch;
 	return dd;
 }
