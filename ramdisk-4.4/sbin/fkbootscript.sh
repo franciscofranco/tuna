a9b4c7afb18b70ecacc8a638d6639d2313c20f38
osm0sis <osm0sis@outlook.com>
2014-08-21 23:27:57 -0300

    Initial commit - track ramdisk-4.3, ramdisk-4.4, and zipfiles on their own branch to keep things clean - rebase ramdisk-4.4 to fix CM-based SELinux breakage of paid apps - move most tweaks to bootscripts to keep things simple

---
+++ ramdisk-4.4/sbin/fkbootscript.sh
@@ -0,0 +1,90 @@
+#!/system/bin/sh
+# Franco's Dev Team
+# malaroth, osm0sis, joaquinf, The Gingerbread Man, pkgnex, Khrushy, shreddintyres
+
+# custom busybox installation shortcut
+bb=/sbin/bb/busybox;
+
+# disable sysctl.conf to prevent ROM interference with tunables
+$bb mount -o rw,remount /system;
+$bb [ -e /system/etc/sysctl.conf ] && $bb mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
+
+# disable the PowerHAL since there is now a kernel-side touch boost implemented
+$bb [ -e /system/lib/hw/power.tuna.so.fkbak ] || $bb cp /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.fkbak;
+$bb [ -e /system/lib/hw/power.tuna.so ] && $bb rm /system/lib/hw/power.tuna.so;
+
+# create and set permissions for /system/etc/init.d if it doesn't already exist
+if [ ! -e /system/etc/init.d ]; then
+  $bb mkdir /system/etc/init.d;
+  $bb chown -R root.root /system/etc/init.d;
+  $bb chmod -R 755 /system/etc/init.d;
+fi;
+$bb mount -o ro,remount /system;
+
+# disable debugging
+echo "0" > /sys/module/wakelock/parameters/debug_mask;
+echo "0" > /sys/module/userwakelock/parameters/debug_mask;
+echo "0" > /sys/module/earlysuspend/parameters/debug_mask;
+echo "0" > /sys/module/alarm/parameters/debug_mask;
+echo "0" > /sys/module/alarm_dev/parameters/debug_mask;
+echo "0" > /sys/module/binder/parameters/debug_mask;
+
+# initialize timer_slack
+echo 100000000 > /dev/cpuctl/apps/bg_non_interactive/timer_slack.min_slack_ns;
+
+# decrease fs lease time
+echo 10 > /proc/sys/fs/lease-break-time;
+
+# tweak for slightly larger kernel entropy pool
+echo 128 > /proc/sys/kernel/random/read_wakeup_threshold;
+echo 256 > /proc/sys/kernel/random/write_wakeup_threshold;
+
+# disabled ASLR to increase AEM-JIT cache hit rate
+echo 0 > /proc/sys/kernel/randomize_va_space;
+
+# general queue tweaks
+for i in /sys/block/*/queue; do
+  echo 512 > $i/nr_requests;
+  echo 512 > $i/read_ahead_kb;
+  echo 2 > $i/rq_affinity;
+  echo 0 > $i/nomerges;
+  echo 0 > $i/add_random;
+  echo 0 > $i/rotational;
+done;
+
+# remount sysfs+sdcard with noatime,nodiratime since that's all they accept
+$bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /;
+$bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /proc;
+$bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /sys;
+$bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /sys/kernel/debug;
+$bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /mnt/shell/emulated;
+for i in /storage/emulated/*; do
+  $bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto $i;
+  $bb mount -o remount,nosuid,nodev,noatime,nodiratime -t auto $i/Android/obb;
+done;
+
+# wait for systemui and increase its priority
+while sleep 1; do
+  if [ `$bb pidof com.android.systemui` ]; then
+    systemui=`$bb pidof com.android.systemui`;
+    $bb renice -18 $systemui;
+    $bb echo -17 > /proc/$systemui/oom_adj;
+    $bb chmod 100 /proc/$systemui/oom_adj;
+    exit;
+  fi;
+done&
+
+# lmk whitelist for common launchers and increase launcher priority
+list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.mycolorscreen.themer com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam";
+while sleep 60; do
+  for class in $list; do
+    if [ `$bb pgrep $class | head -n 1` ]; then
+      launcher=`$bb pgrep $class`;
+      $bb echo -17 > /proc/$launcher/oom_adj;
+      $bb chmod 100 /proc/$launcher/oom_adj;
+      $bb renice -18 $launcher;
+    fi;
+  done;
+  exit;
+done&
+
