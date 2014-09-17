05ca9dfb5013d5cd52d4cf0cbf7711b028c4e68c
franciscofranco <franciscofranco.1990@gmail.com>
2014-09-17 04:02:21 +0100

    Revert "omap2: overclock ram."
    This reverts commit 67202b3e93f9a20624f0f30296fc272010604d9e.

--- arch/arm/mach-omap2/include/mach/emif.h
+++ arch/arm/mach-omap2/include/mach/emif.h
@@ -21,7 +21,7 @@
 #define EMIF2	1
 
 /* The maximum frequency at which the LPDDR2 interface can operate in Hz*/
-#define MAX_LPDDR2_FREQ	533000000	/* 400 MHz */
+#define MAX_LPDDR2_FREQ	400000000	/* 400 MHz */
 
 /* 19.2 MHz to be used for finding initialization values */
 #define EMIF_FREQ_19_2_MHZ 19200000	/* 19.2 MHz */
@@ -40,7 +40,7 @@
  * Determines the number of entries in the pointer array for register
  * cache
  */
-#define EMIF_MAX_NUM_FREQUENCIES	12
+#define EMIF_MAX_NUM_FREQUENCIES	6
 /*
  * Indices into the Addressing Table array.
  * One entry each for all the different types of devices with different
